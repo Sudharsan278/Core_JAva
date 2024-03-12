ce06e36d904057de8c273332028208be04cc0513
sudha <sudha@Sudharsan>
2024-03-12 19:24:42 +0530

    This is my java programs

---
+++ Core_JAva/src/Example_Programs/Person_Demo.java
@@ -0,0 +1,32 @@
+//Driver Program 
+package Example_Programs;
+
+import java.util.Scanner;
+
+public class Person_Demo
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name of the Perosn: ");
+		String name = sc.nextLine();
+		System.out.println("Enter the age of the Person: ");
+		int age = sc.nextInt();
+		System.out.println("Enter the Gender: ");
+		String gender = sc.next();
+		System.out.println("Enter the Income: ");
+		int income = sc.nextInt();
+		
+		Person p = new Person();
+		p.setName(name);
+		p.setAge(age);
+		p.setGender(gender);
+		p.setIncome(income);
+		System.out.println(p);
+		Tax_Calculation tax = new Tax_Calculation();
+		tax.taxCalculation(p);
+		System.out.println("After Calculating the tax: ");
+		System.out.println(p);
+		sc.close();
+	}
+}
